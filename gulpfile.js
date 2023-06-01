0adfa05b06b97bd3c2e8f8b020fbcab12851b3e9
Hamadn <nhamad919@gmail.com>
2023-06-01 17:17:50 +0400

    gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const {src, dest, watch, series} = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const terser = require('gulp-terser');
+
+//Sass Task 
+function scssTask(){
+    return src('scss/style.scss', {sourcemaps: true})
+    .pipe(sass())
+    .pipe(postcss([cssnano()]))
+    .pipe(dest('dist', {sourcemaps: '.'}));
+    scssTask();
+}
+
+exports.default = series(scssTask);
